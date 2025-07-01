991554fd50a44ca972602ef54873cc5655c5597e
Aviv3434 <aviv3434@gmail.com>
2025-07-01 05:45:15 +0300

    Added WorkerManager.. still not Syncing. need to fix this

--- app/src/main/java/com/example/myapplication/worker/SyncWorker.kt
+++ app/src/main/java/com/example/myapplication/worker/SyncWorker.kt
@@ -3,10 +3,12 @@ package com.example.myapplication.worker
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.example.myapplication.repository.ExchangeRateRepository
 
 class SyncWorker(
     context: Context,
-    params: WorkerParameters
+    params: WorkerParameters,
+    repository: ExchangeRateRepository
 ) : CoroutineWorker(context, params) {
     override suspend fun doWork(): Result {
         return Result.success()
